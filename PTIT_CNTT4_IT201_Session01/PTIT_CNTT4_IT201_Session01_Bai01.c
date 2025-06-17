d176f2855fce35d9d12230471f84859641827708
duydep24022006 <memay26032006@gmail.com>
2025-06-17 10:58:13 +0700

    moio

--- PTIT_CNTT4_IT201_Session01/PTIT_CNTT4_IT201_Session01_Bai01.c
+++ PTIT_CNTT4_IT201_Session01/PTIT_CNTT4_IT201_Session01_Bai01.c
@@ -2,9 +2,8 @@
 
 int main(int n) {
     for (int i=0; i < n; i++) {//độ phức tạp là O(n);
-        printf("hello\n");//độ phức tạp là O(1);
+        printf("hello\n");//độ phức tạp là O(n);
     }
-
     return 0;
     //độ phức tạp thời gian của là  O(n);
 }
--- PTIT_CNTT4_IT201_Session01/PTIT_CNTT4_IT201_Session01_Bai10.c
+++ PTIT_CNTT4_IT201_Session01/PTIT_CNTT4_IT201_Session01_Bai10.c
@@ -1,14 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
+
+#define CHAR_RANGE 256
+
+
+bool isAnagram_Counting(const char *str1, const char *str2) {
+    // Nếu độ dài khác nhau thì chắc chắn không phải hoán vị
+    if (strlen(str1) != strlen(str2)) return false;
+
+    int count[CHAR_RANGE] = {0};//O(256):vì nó chưa 256 phàn tử
+
+
+    for (int i = 0; str1[i]; i++) {//O(n):vì mảng này nó chạy từ 0 tới n phàn tử của mảng
+        count[(unsigned char)str1[i]]++;
+    }
+
+
+    for (int i = 0; str2[i]; i++) {//O(n):vì mảng này nó chạy từ 0 tới n phàn tử của mảng
+        count[(unsigned char)str2[i]]--;
+    }
+
+
+    for (int i = 0; i < CHAR_RANGE; i++) {//O(n):vì mảng này nó chạy từ 0 tới n phàn tử của mảng
+        if (count[i] != 0) return false;
+    }
+
+    return true;
+}
+/*-độ phức tạp thời gian là O(n):vì O(n) + O(n) + O(256) → O(n);
+* -độ phức tạp không gian là O(1);
+*/
 
 int main() {
-    char str1[100] ="hello";
+    char str1[100];
     char str2[100];
 
+    printf("Moi ban nhap chuoi dau tien\n");
+    fgets(str1, sizeof(str1), stdin);
+    printf("Moi ban nhap chuoi thu hai\n");
+    fgets(str2, sizeof(str2), stdin);
 
-
-}
-
-char str(char str1[]) {
+    if (isAnagram_Counting(str1 ,str2) == true ) {
+        printf("True\n");
+    }else {
+        printf("False\n");
+    }
 
 }
